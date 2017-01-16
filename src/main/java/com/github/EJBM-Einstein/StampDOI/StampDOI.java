0950f5eaf8e99332690d044b53dfba53281a4e13
mes32 <stockman.mike@gmail.com>
2017-01-16 13:34:06 -0500

    feature: Configure project structure and setup gradle build tool

---
+++ src/main/java/com/github/EJBM-Einstein/StampDOI/StampDOI.java
@@ -0,0 +1,10 @@
+package com.github.EJBM_Einstein.StampDOI;
+
+import java.io.*;
+import org.apache.pdfbox.io.*;
+
+class StampDOI {
+    public static void main(String[] args) {
+        System.out.println("Hello Stamp DOI");
+    }
+}
